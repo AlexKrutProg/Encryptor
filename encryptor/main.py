a59d43fbea92ab990be8cc3d32a5d9de2fc974d5
alexkrut <a.d.ivanov@phystech.edu>
2022-03-29 21:58:59 +0300

    First version

---
+++ encryptor/main.py
@@ -0,0 +1,33 @@
+import sys
+import argparse
+from encryptor import Encryptor, InfoManager
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command')
+    parser.add_argument('--cipher', default="caesar")
+    parser.add_argument('--key', default="1")
+    parser.add_argument('--input_file', default=None)
+    parser.add_argument('--output_file', default=None)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    manager = InfoManager(namespace.input_file, namespace.output_file)
+    manager.read_text()
+
+    if namespace.command == "encode":
+        if namespace.cipher == "caesar":
+            manager.text = [Encryptor.encode_word_caesar(line, namespace.key) for line in manager.text]
+        elif namespace.cipher == "vigenere":
+            manager.text = [Encryptor.encode_word_vigenere(line, namespace.key) for line in manager.text]
+    elif namespace.command == "decode":
+        if namespace.cipher == "caesar":
+            manager.text = [Encryptor.decode_word_caesar(line, namespace.key) for line in manager.text]
+        elif namespace.cipher == "vigenere":
+            manager.text = [Encryptor.decode_word_vigenere(line, namespace.key) for line in manager.text]
+
+    manager.write_text()
